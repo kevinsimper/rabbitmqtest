a87da46b2d275df3fcb92538dbe00a08b561117b
Kevin Simper <kevin.simper@gmail.com>
2020-06-30 10:19:10 +0200

    Try out recursion with amqplib

--- src/connect.js
+++ src/connect.js
@@ -1,12 +1,19 @@
 const amqplib = require("amqplib");
 exports.connect = async (url) => {
-  return amqplib.connect(url);
+  return await amqplib.connect(url);
 };
 
-exports.connectRetry = async (url, retry) => {
-  const connection = await amqplib.connect(url);
-  connection.on("error", () => {
-    console.log("something");
-  });
-  return connection;
+exports.connectRetry = async (url, retry, call) => {
+  let called = 0;
+  async function con() {
+    const connection = await amqplib.connect(url).catch(() => {
+      return con();
+    });
+    connection.on("error", () => {
+      console.log("something");
+      called++;
+    });
+    return connection;
+  }
+  return await con();
 };
