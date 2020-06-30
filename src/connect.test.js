a55ff42b53f9aff29fc3445b3d2533cc9a14c080
Kevin Simper <kevin.simper@gmail.com>
2020-06-30 10:03:47 +0200

    Add connection with retry

--- src/connect.test.js
+++ src/connect.test.js
@@ -1,10 +1,23 @@
 jest.mock("amqplib");
 const amqplib = require("amqplib");
-const { connect } = require("./connect");
+const { connect, connectRetry } = require("./connect");
 
 test("amqplib connect", async () => {
   amqplib.connect.mockResolvedValueOnce({});
   const connection = await connect("amqp://localhost");
   expect(connection).toBeDefined();
   expect(amqplib.connect).toHaveBeenCalled();
+});
+
+test("amqplib makes an error", async () => {
+  let events = {};
+  const on = jest.fn((name, callback) => {
+    events[name] = callback;
+  });
+  amqplib.connect.mockResolvedValueOnce({
+    on,
+  });
+  const connection = await connectRetry("amqp://localhost", 5);
+  expect(on).toHaveBeenCalled();
+  expect(events.error).toBeInstanceOf(Function);
 });
